392a2b6b4ea84cbba31a8e4c7e9eb98a6d3f22bc
Lauren Kemperman <lkemperm@u.rochester.edu>
2016-11-18 10:40:49 -0500

    inquiry and events table exploration
    Working on converting character data to time date format to find
    duration, converting data to formats that make it easier to work with.

--- inquiry_event_exploration.R
+++ inquiry_event_exploration.R
@@ -2,26 +2,25 @@
 inquiry_table <- read.csv('data/merged_inquiries.csv', stringsAsFactors = FALSE)
 # drop columns that we are not interested in for now from inquiry table 
 inquiry_table <- subset(inquiry_table, select = -c(inquiryId, certificateId, serverName, eventId, clientId, answerDate))
-inquiry_table$startTime <- as.POSIXct(as.numeric(inquiry_table$startTime), origin = "1970-01-01", na.rm = TRUE)
-inquiry_table$endTime <- as.POSIXct(as.numeric(inquiry_table$endTime), origin = "1970-01-01", na.rm = TRUE)
+# this cuts off the time 
+inquiry_table$startTime <- format(inquiry_table$startTime, format="%m-%d-%Y-%R%p")
+inquiry_table$endTime <- format(inquiry_table$endTime, format="%m-%d-%Y-%R%p")
 # create column for duration 
 inquiry_table["duration"] = (inquiry_table$endTime - inquiry_table$startTime)
 # replace null with none for search queries 
 inquiry_table$searchQuery[inquiry_table$searchQuery == "NULL"] <- "NONE"
+# histogram of inquiry duration after turning data into numeric
+inquiry_table$duration <- as.numeric(inquiry_table$duration)
+hist(inquiry_table$duration)
 # try: making word cloud for search queries 
 # simplify text data 
 library(tm)
 library(SnowballC)
 library(wordcloud)
-library(RColorBrewer)
-# searchText <- c("hello VisualDX project test", "project test", "project test test ")
-# searchQueryCorpus <- Corpus(VectorSource(searchText))
-searchVector <- inquiry_table[, "searchQuery", drop = FALSE]
-topQueries <- sort(table(searchVector), decreasing = TRUE)[1:50]
-topQueries <- topQueries[-1]
-topVector <- rep(names(topQueries), topQueries[names(topQueries)])
-# remove "NONE" observations 
-searchQueryCorpus <- Corpus(VectorSource(topVector))
+searchText <- c("hello VisualDX project test", "project test", "project test test ")
+searchQueryCorpus <- Corpus(VectorSource(searchText))
+searchVector <- inquiry_table[, inquiry_table$searchQuery]
+testCorpus <- Corpus(VectorSource(searchMatrix))
 searchQueryCorpus <- tm_map(searchQueryCorpus, PlainTextDocument)
 searchQueryCorpus <- tm_map(searchQueryCorpus, removePunctuation)
 searchQueryCorpus <- tm_map(searchQueryCorpus, removeWords, stopwords('english'))
@@ -41,6 +40,11 @@ sum((events_table$diagnosisId) == "NULL")
 # replace null values with 0, otherwise 1 (8502938 null objects)
 events_table$imageId[events_table$imageId != "NULL"] <- 1
 events_table$imageId[events_table$imageId == "NULL"] <- 0
+events_table$imageId <- as.numeric(events_table$imageId)
 # do the same for diagnosis ID 
 events_table$diagnosisId[events_table$diagnosisId != "NULL"] <- 1
 events_table$diagnosisId[events_table$diagnosisId == "NULL"] <- 0
+events_table$diagnosisId <- as.numeric(events_table$diagnosisId)
+# many null controlIds: may be easier to interpret as 0's 
+events_table$controlId[events_table$controlId == "NULL"]<- 0
+
