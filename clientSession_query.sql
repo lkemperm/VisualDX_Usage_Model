f88b8f9a3f9f8da63f8feca1a647439400a3ac65
jchakmak <jmchakmakas@gmail.com>
2016-11-20 22:27:57 -0500

    2 figures, an R file, and updated documentation

--- clientSession_query.sql
+++ clientSession_query.sql
@@ -4,17 +4,54 @@ WHERE (YEAR(modTime) = 2014
 OR YEAR(modTime) = 2015 OR YEAR(modTime) = 2016);
 
 
-##joining with WHERE
-SELECT *
-FROM authorization.ClientSession cs, authorization.Contract c
-WHERE (cs.contractId = c.contractId AND c.contractTypeId = 1);
-
-##inner join
+##joining ClientSession and Contract to get contractTypeId
 SELECT *
 FROM authorization.ClientSession cs
 INNER JOIN authorization.Contract c
 ON cs.contractId = c.contractId
-WHERE (c.contractTypeId = 1);
+WHERE (c.contractTypeId = 1 AND rand() <= 0.2);
+
+SELECT *
+FROM authorization.ClientSession cs
+INNER JOIN authorization.Contract c
+ON cs.contractId = c.contractId
+WHERE (c.contractTypeId = 2 AND rand() <= 0.2);
+
+SELECT *
+FROM authorization.ClientSession cs
+INNER JOIN authorization.Contract c
+ON cs.contractId = c.contractId
+WHERE (c.contractTypeId = 3);
+
+SELECT *
+FROM authorization.ClientSession cs
+INNER JOIN authorization.Contract c
+ON cs.contractId = c.contractId
+WHERE (c.contractTypeId = 4);
+
+SELECT *
+FROM authorization.ClientSession cs
+INNER JOIN authorization.Contract c
+ON cs.contractId = c.contractId
+WHERE (c.contractTypeId = 5);
+
+SELECT *
+FROM authorization.ClientSession cs
+INNER JOIN authorization.Contract c
+ON cs.contractId = c.contractId
+WHERE (c.contractTypeId = 6);
+
+SELECT *
+FROM authorization.ClientSession cs
+INNER JOIN authorization.Contract c
+ON cs.contractId = c.contractId
+WHERE (c.contractTypeId = 7);
+
+SELECT *
+FROM authorization.ClientSession cs
+INNER JOIN authorization.Contract c
+ON cs.contractId = c.contractId
+WHERE (c.contractTypeId = 8);
 
 
 
